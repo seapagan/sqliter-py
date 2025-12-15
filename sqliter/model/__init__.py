ddd9d065276072aa713aadf220c33a35158402ba
Grant Ramsay <seapagan@gmail.com>
2025-12-15 09:49:40 +0000

    keep old 'Unique' for now as well as new 'unique'
    this allows complete backward compatibility until it is removed
    
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- sqliter/model/__init__.py
+++ sqliter/model/__init__.py
@@ -1,11 +1,14 @@
 """This module provides the base model class for SQLiter database models.
 
 It exports the BaseDBModel class, which is used to define database
-models in SQLiter applications, and the Unique class, which is used to
+models in SQLiter applications, and the unique function, which is used to
 define unique constraints on model fields.
 """
 
 from .model import BaseDBModel, SerializableField
 from .unique import unique
 
-__all__ = ["BaseDBModel", "SerializableField", "unique"]
+# Backward compatibility alias (deprecated, will be removed in v1.0.0)
+Unique = unique
+
+__all__ = ["BaseDBModel", "SerializableField", "unique", "Unique"]
